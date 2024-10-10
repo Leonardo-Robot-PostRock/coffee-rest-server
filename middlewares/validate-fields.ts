7fd568cea0e3327d2e75140a1737b1e79f3dd318
Leonardo <leonardo38361@gmail.com>
2024-10-10 20:29:27 -0300

    ADD: validateFields function

--- middlewares/validate-fields.ts
+++ middlewares/validate-fields.ts
@@ -1,15 +1,12 @@
 import { NextFunction, Request, Response } from "express";
 import { validationResult } from "express-validator";
 
-const validateFields = (req: Request, res: Response, next: NextFunction) => {
+export const validateFields = (req: Request, res: Response, next: NextFunction): void => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json(errors);
+        res.status(400).json(errors);
+        return;
     }
 
     next();
-}
-
-module.exports = {
-    validateFields
 }
