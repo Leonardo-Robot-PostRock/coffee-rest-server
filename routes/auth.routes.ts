e8d1752e905bc71eaca06a90190b10811d0c4084
Leonardo <leonardo38361@gmail.com>
2024-10-10 20:35:02 -0300

    ADD: login route

--- routes/auth.routes.ts
+++ routes/auth.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import { check, query } from 'express-validator';
 import { validateFields } from '../middlewares/validate-fields';
+import { login } from '../controllers/auth.controller';
 
 const router = Router();
 
-router.post('/',)
+router.post('/login', [
+    check('email', 'El correo debe ser obligatorio').isEmail(),
+    check('password', 'La contraseña es obligatoria').not().isEmpty(),
+    validateFields], login)
 
-module.exports = router;
+export default router;
