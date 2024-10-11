534648c739bd67eb4956dfcf25adf0a248054735
Leonardo <leonardo38361@gmail.com>
2024-10-11 12:25:22 -0300

    ADD: validar usuario

--- middlewares/validate-jwt.ts
+++ middlewares/validate-jwt.ts
@@ -1,7 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 import jwt, { JwtPayload, PublicKey, Secret } from 'jsonwebtoken';
+import User from '../models/user';
 
-export const validateJWT = (req: Request, res: Response, next: NextFunction) => {
+export const validateJWT = async (req: Request, res: Response, next: NextFunction) => {
 
     const token = req.header('x-token');
 
@@ -18,6 +19,22 @@ export const validateJWT = (req: Request, res: Response, next: NextFunction) => 
             const { uid } = payload as JwtPayload;
 
             req.uid = uid;
+
+            const user = await User.findById(uid);
+
+            if (!user) {
+                return res.status(400).json({
+                    msg: 'Token no válido - usuario no existe en DB'
+                })
+            }
+
+            if (!user.state) {
+                return res.status(401).json({
+                    msg: 'Token no válido'
+                })
+            }
+
+            req.user = user;
 
             next();
         } else {
