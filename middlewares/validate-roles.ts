a268e0cc2ae6ba3f77b9cc8cf56722a134048fc2
Leonardo <leonardo38361@gmail.com>
2024-10-14 16:11:31 -0300

    ADD: middleware con argumentos para roles

--- middlewares/validate-roles.ts
+++ middlewares/validate-roles.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import { IUser } from "../interfaces/user";
 
 
 export const isAdminRole = (req: Request, res: Response, next: NextFunction) => {
@@ -17,4 +18,24 @@ export const isAdminRole = (req: Request, res: Response, next: NextFunction) => 
     }
 
     next();
+}
+
+export const hasRole = (...roles: IUser['role'][]) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+
+
+        if (!req.user) {
+            return res.status(500).json({
+                msg: 'Se quiere verificar el role sin verificar el token primero'
+            })
+        }
+
+        if (!roles.includes(req.user.role)) {
+            return res.status(401).json({
+                msg: `El servicio requiere uno de estos roles: ${roles}`
+            })
+        }
+
+        next();
+    }
 }
