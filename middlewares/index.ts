95e61d430dfbe6974fc61f5e7c1858be05360f8f
Leonardo <leonardo38361@gmail.com>
2024-10-16 11:21:00 -0300

    ADD: archivo de importaciones

---
+++ middlewares/index.ts
@@ -0,0 +1,3 @@
+export { validateFields } from '../middlewares/validate-fields';
+export { hasRole, isAdminRole } from '../middlewares/validate-roles';
+export { validateJWT } from '../middlewares/validate-jwt';
