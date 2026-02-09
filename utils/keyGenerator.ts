f372965861442ebdc135997cfb3fc672c57ce7f0
Leonardo <leonardo38361@gmail.com>
2026-02-09 19:42:11 -0300

    delete: key generator function

--- utils/keyGenerator.ts
+++
@@ -1,9 +0,0 @@
-import { randomBytes } from 'crypto';
-
-const generateSecretKey = () => {
-    return randomBytes(64).toString('base64');
-};
-
-const secretKey = process.env.SECRETORPRIVATEKEY || generateSecretKey();
-
-export { secretKey };
