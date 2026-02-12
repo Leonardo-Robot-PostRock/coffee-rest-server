92783b67fc93f373e00d6f283aaaab3f9321e346
Leonardo <leonardo38361@gmail.com>
2026-02-11 21:13:49 -0300

    refactor: imports and add comments

--- routes/auth.ts
+++ routes/auth.ts
@@ -1,14 +1,15 @@
 import { Router } from 'express';
 import { check } from 'express-validator';
-import { validateFields } from '../helpers/validate-fields';
 import { googleSignIn, login } from '../controllers/auth';
+import { validateFields } from '../helpers/db-validators';
 
 const router = Router();
 
 router.post('/login', [
     check('email', 'El correo debe ser obligatorio').isEmail(),
     check('password', 'La contraseña es obligatoria').not().isEmpty(),
-    validateFields], login);
+    validateFields
+], login);
 
 router.post('/google', [
     check('id_token', 'id_token es necesario').not().isEmpty(),
