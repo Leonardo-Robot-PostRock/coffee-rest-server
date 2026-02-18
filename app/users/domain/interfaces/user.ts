5f018a5300ffcd0069399d7ed71297813759a9b3
Leonardo <leonardo38361@gmail.com>
2026-02-17 21:30:23 -0300

    chore: interface de user

---
+++ app/users/domain/interfaces/user.ts
@@ -0,0 +1,11 @@
+export interface IUser {
+    name: string;
+    email: string;
+    password: string;
+    img?: string;
+    role?: string;
+    state?: boolean;
+    google?: boolean;
+    created_at?: Date;
+    updated_at?: Date;
+}
