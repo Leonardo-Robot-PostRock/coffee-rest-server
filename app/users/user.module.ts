e9c69b173d1136575f23482f56f9edc7af2d10ed
Leonardo <leonardo38361@gmail.com>
2026-02-18 03:19:55 -0300

    chore: module composition to build user controller

---
+++ app/users/user.module.ts
@@ -0,0 +1,22 @@
+// Infrastructure
+import { MongooseUserRepository } from "./infrastructure/mongoose/user.repository";
+import { BcryptPasswordHasher } from "./infrastructure/security/bcrypt-password-hasher";
+
+// User Service Layer
+import { UserService } from "./services/user.service";
+
+// Presentation
+import { buildUserController } from "./presentation/api/user.controller";
+
+// Module Composition
+const userRepository = new MongooseUserRepository();
+const passwordHasher = new BcryptPasswordHasher();
+
+// Service
+const userService = new UserService(
+  userRepository,
+  passwordHasher
+);
+
+// Controller already built with the service
+export const userController = buildUserController(userService);
