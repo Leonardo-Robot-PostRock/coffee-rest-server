8a14dea566097f3a394ba6e0edd04c76f06fdedc
Leonardo <leonardo38361@gmail.com>
2026-02-19 06:22:38 -0300

    refactor: cambio de ruta

--- app/auth/infrastructure/security/bcrypt-password-hasher.ts
+++
@@ -1,9 +0,0 @@
-import bcrypt from 'bcryptjs';
-import { IPasswordHasher } from '../../domain/interfaces/password-hasher';
-
-export class BcryptPasswordHasher implements IPasswordHasher {
-  async hash(password: string): Promise<string> {
-    const salt = bcrypt.genSaltSync();
-    return bcrypt.hashSync(password, salt);
-  }
-}
