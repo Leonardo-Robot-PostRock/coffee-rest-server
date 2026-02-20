e096b2ade63735f380bdf74c4c20ed1f5885f9b4
Leonardo <leonardo38361@gmail.com>
2026-02-20 18:46:20 -0300

    refactor: add comment

--- app/shared/handle-duplicate-error.ts
+++ app/shared/handle-duplicate-error.ts
@@ -1,5 +1,6 @@
 import { MongoServerError } from 'mongodb';
 
+// Translate the MongoDB duplicate key technical error into a more specific domain error for the application
 export function handleDuplicateError(error: unknown, value: string, ErrorClass: new (value: string) => Error): never {
     if (error instanceof MongoServerError && error.code === 11000) {
         throw new ErrorClass(value);
